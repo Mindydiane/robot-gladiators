063c38a73d5c137908c7d603e42d918b466d63cf
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-05-19 15:13:07 -0500

    initiate js process

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+//this creates a prompt for robots name 
+var playerName = window.prompt("What is your robot's name?");
+// test without alert
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message")
+// this will do math and log 20
+console.log(10 + 10);
+// robot's name is a string concatenation
+console.log("Our robot's name is " + playerName);
+
+//this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
