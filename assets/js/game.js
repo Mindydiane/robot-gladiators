e78bd88985267186943dabd526ae31ee7f80e126
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-11-06 01:00:32 -0500

    added more to console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,16 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+console.log("Your bots name is " + playerName);
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+console.log(playerName +" attack amount is "+ playerAttack, " remaining health is " + playerHealth);
 
 var enemyName = "Robort";
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(playerName + " VS " + enemyName);
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
