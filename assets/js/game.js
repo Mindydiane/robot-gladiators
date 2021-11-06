b37f6917fd1fd1e7290e355742f6cfe41263a781
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-11-06 00:29:47 -0500

    console log examples

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,10 @@
 var playerName = window.prompt("what is your robot's name?");
 console.log(playerName);
-// Note the lack of "quotation marks" around playerName due to it being a variable
-window.alert(playerName)
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
 
 // this creates a function named "fight"
 function fight() {
