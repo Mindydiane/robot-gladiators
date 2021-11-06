b334a3a0e436c99a653f9c2b7b31d88d43e1ede5
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-11-06 00:02:59 -0500

    window prompt for robot's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,8 @@
-// this creates a function named "fight"
+window.prompt("what is your robot's name?");
 
+// this creates a function named "fight"
 function fight() {
     window.alert("The fight has begun, Protect your honor!");
 }
 
-fight ();
+fight();
