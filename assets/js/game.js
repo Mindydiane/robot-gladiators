72d7f055314c90306d09d75570c3bd4677d4809f
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-11-06 00:12:37 -0500

    created variable to hold player name w/log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,12 @@
-window.prompt("what is your robot's name?", "");
+var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
 
 // this creates a function named "fight"
 function fight() {
     window.alert("The fight has begun, Protect your honor!");
 }
+
+
+
 
 fight();
